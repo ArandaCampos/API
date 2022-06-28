62a1eb6e4cadcb1ba293891ea4d3fb71d352dea4
ArandaCampos <renan.aranda.campos.rac@gmail.com>
2022-06-27 23:15:36 -0300

    styled add relatorio

--- cliente/src/routes/Relatorio.js
+++ cliente/src/routes/Relatorio.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect} from "react";
 import Api from "../api";
+import styled from "styled-components";
 
 export default function Relatorio(){
     const [dados, setDados] = useState()
@@ -26,31 +27,103 @@ export default function Relatorio(){
 
 
     return(
-        <>
-            <h1>Relatorio</h1>
-            <table>
-                <tr>
-                    <th>Tipo</th>
-                    <th>Clientes</th>
-                </tr>
+        
+        <Center>
+            <Title>Relatório</Title>
+            {error ? <Alert>{error}</Alert> : ''}
+            <Table>
+                <HeaderTable>
+                    <FieldTable>Tipo</FieldTable>
+                    <FieldTable>Clientes</FieldTable>
+                </HeaderTable>
             {dados && dados.map((dado) => (
-                <tr>
-                    <td>{dado.tipo}</td>
-                    <tr>
+                <BodyTable>
+                    <FieldTable>{dado.tipo}</FieldTable>
+                    <BodyTable>
                         {dado.clientes.map( (cliente) => (
-                        <td>{cliente.cliente}</td>
+                            <FieldTable>{cliente.cliente}</FieldTable>
                         ))}
-                    </tr>
-                </tr>
+                    </BodyTable>
+                </BodyTable>
             ))}
-            </table>
+            </Table>
+        
+            <Column>
             {exportacao && exportacao.map( (count) => (
-                <>
-                    <p>{count.tipo}</p>
-                    <p>{count.quantia}</p>
-                </>
+                <Linear>                    
+                    <h5>{count.tipo }</h5>
+                    <h5>|</h5>
+                    <h5>{count.quantia}</h5>
+                </Linear>
             ))}
-        </>
+            </Column>
+        </Center>    
     )
 
 }
+
+const Linear = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-end;
+    background-color: #04BF9D;
+    padding: 5px 10px;
+    
+    color: white;
+    height: 30px;
+    gap: 10px;
+`;
+
+const Column = styled.div`
+    margin-top: 35px; 
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+`;
+const Link = styled.a`
+    text-decoration: none;
+    color: white;
+`;
+
+const Table = styled.table`
+    margin-top: 30px;
+    border-collapse: separate;
+    border-spacing: 0px;
+`;
+const HeaderTable = styled.tr`
+    background-color: #04BF9D;
+    color: black;
+`;
+
+const BodyTable = styled.tr`
+    background-color: #DEEFE7;
+    color:black;
+`;
+
+const FooterTable = styled.tr`
+    background-color: #DEEFE7;
+    color:black;
+`;
+
+
+const FieldTable = styled.th`
+    padding: 10px 20px;
+`;
+
+const Title = styled.h1`
+    color: black;
+    font-size: 25px;
+`;
+
+const Alert = styled.p`
+    color: red;
+    font-size: 15px;
+`;
+
+const Center = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
